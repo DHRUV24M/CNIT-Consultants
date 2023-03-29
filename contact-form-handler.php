fd16a4d1c2196b4b663adbb84401a3e4379fa3ed
DHRUV24M <dhruvmanral9@gmail.com>
2023-03-29 21:34:39 +0530

    minor chnges in spell

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,29 +1,43 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // form has been submitted
 
 
-    $email_from = 'info@easytutorialspro.com';
+    // retrieve data using post request and sanitize it to prevent SQL injection attacks and other vulnrebilties
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-    $email_subject = "New Form Submission";
-    
-    $email_body =   "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                    "Subject: $subject.\n".
-                    "User Message: $message.\n";
+    // validate the data and look if it meets the form requrements
+    if (empty($name) || empty($email) || empty($message)) {
+        // one or more fields are empty
+      } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // email address is not valid
+      } elseif (strlen($message) < 10) {
+        // message is too short
+      } else {
+        // form data is valid, proceed with processing
+      }
 
+      // if form is valid then send an email and store in database
+        $to = "youremail@example.com";
+        $subject = "New message from " . $name;
+        $body = "Name: " . $name . "\n\n" . "Email: " . $email . "\n\n" . "Message: " . $message;
+        $headers = "From: " . $email;
 
-    $to = "avinash6252@gmail.com";
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Thank you for your message!";
+        } 
+        else {
+            echo "An error occurred while sending your message.";
+        }
 
-    $headers = "From: $email_from \r\n";
-
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: contact.html");
-
-
+        // message to user if the form is sucessfully submited or not
+        if (mail($to, $subject, $body, $headers)) {
+            echo "<p>Thank you for your message!</p>";
+        } else {
+            echo "<p>An error occurred while sending your message. Please try again later.</p>";
+            }
+}
 ?>
